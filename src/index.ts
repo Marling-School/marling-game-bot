30541d2b786d500ea3e223d8c611bc52c39ed882
Joe Sharp <ratracejoe@hotmail.com>
2021-01-31 21:48:17 +0000

    basic maths game

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as logger from 'winston'
-import { Client } from 'discord.js';
+import { Client, Message } from 'discord.js';
 import dotenv from 'dotenv'
 
 // Load environment variables
@@ -17,9 +17,58 @@ client.on('ready', () => {
     logger.info(`Logged in as ${client.user && client.user.tag}!`);
 });
 
+const SIGNAL = '>'
+type PendingHandler = (msg: Message) => boolean;
+let pendingHandler: PendingHandler | undefined;
+
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+    if (msg.author.bot) return;
+
+    if (!msg.content.startsWith(SIGNAL)) {
+        // If we are waiting for a response to something
+        if (!!pendingHandler) {
+            // If this message satisfies that response
+            if (pendingHandler(msg)) {
+                // We can remove the pending handler
+                pendingHandler = undefined;
+            }
+        }
+        return
+    }
+
+    const message = msg.content.slice(1);
+    const parts = message.split(' ');
+
+    if (parts.length > 0) {
+        switch (parts[0]) {
+            case 'play':
+                msg.channel.send('Welcome to Maths Game')
+
+                const a = 1 + Math.floor(Math.random() * 10);
+                const b = 1 + Math.floor(Math.random() * 10);
+                const question = `What is ${a} * ${b}?`
+                const answer = (a * b).toString();
+                msg.channel.send(question);
+                pendingHandler = (response: Message) => {
+                    if (response.channel.id === msg.channel.id) {
+                        if (response.content === answer) {
+                            response.channel.send('Correct!')
+                        } else {
+                            response.channel.send('Incorrect :(')
+                        }
+
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                break;
+
+        }
+
+    } else {
+        msg.channel.send('Error - Not enough parts when split on space');
     }
 });
 
